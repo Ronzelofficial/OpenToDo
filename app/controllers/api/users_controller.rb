b6eb4e111b65f8d88b0aa750b682dff71c2476ca
Ronen Reouveni <ronenreouveni@gmail.com>
2018-04-08 14:04:34 -0700

    branch3

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,5 +5,20 @@ class Api::UsersController < ApiController
   def index
     render json: users, each_serializer: UserSerializer
   end
+    def create
+     user = User.new(user_params)
+      if user.save
+      # # 5
+        render json: user
+      else
+      # # 6
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 
 end
