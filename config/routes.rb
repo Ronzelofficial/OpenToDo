e9551916beeb3f1286371d8cd176fd83fc4b9d69
Ronen Reouveni <ronenreouveni@gmail.com>
2018-04-08 12:32:02 -0700

    branch2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'welcome/about'
-
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
